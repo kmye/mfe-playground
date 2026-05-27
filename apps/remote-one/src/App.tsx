efdc0186b63bef0e04b5d53c4b4f63c2e4c37c54
Eileen Kang <eileen_kang@tech.gov.sg>
2026-05-27 20:52:38 +0800

    fix: fix routing for remote apps

--- apps/remote-one/src/App.tsx
+++ apps/remote-one/src/App.tsx
@@ -1,16 +1,26 @@
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link, Outlet} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <Link to=".">Dashboard</Link> | <Link to="settings">Settings</Link>
+      </nav>
+        <Outlet />
+    </div>
+    );
+}
 
 export default function App() {
   return (
     <div>
-      <nav>
-        <Link to="/">Dashboard</Link> | <Link to="/settings">Settings</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Routes>
     </div>
   );
--- apps/remote-two/src/App.tsx
+++ apps/remote-two/src/App.tsx
@@ -1,16 +1,26 @@
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link, Outlet} from "react-router-dom";
 import Overview from "./pages/Overview";
 import Details from "./pages/Details";
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <Link to=".">Overview</Link> | <Link to="details">Details</Link>
+      </nav>
+        <Outlet />
+    </div>
+    );
+}
 
 export default function App() {
   return (
     <div>
-      <nav>
-        <Link to="/">Overview</Link> | <Link to="/details">Details</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<Overview />} />
-        <Route path="/details" element={<Details />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Overview />} />
+          <Route path="details" element={<Details />} />
+        </Route>
       </Routes>
     </div>
   );
